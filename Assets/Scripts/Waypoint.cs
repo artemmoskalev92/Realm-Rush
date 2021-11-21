d743b388f531ec5d8a4f7b3fce66828cfe292c46
artemmoskalev92 <83223168+artemmoskalev92@users.noreply.github.com>
2021-11-21 19:08:41 +0500

    1/2

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject towerPrefab;
+
+    public bool GetIsPlaceable()
+    {
+        return isPlaceable;
+    }
 
     private void OnMouseDown()
     {
         if (isPlaceable)
         {
-            Debug.Log(transform.name);
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
         }
         
     }
